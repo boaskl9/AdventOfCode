44125a4e6d3fe27724281dd3cd30712b8bbdf9c2
unknown <boaskl9@gmail.com>
2025-12-07 15:07:04 +0100

    Part 2 solved, with recurssion and memorization

--- day_7/day_7.py
+++ day_7/day_7.py
@@ -20,21 +20,69 @@ def simulateStep(lines):
         for col in range(len(line)):
             char = line[col]
                         
-            if row > 0:
-                if char == '.' and isLaserAbove(row, col, lines):
-                    print("Case 1")
+            if row > 0: # Don't simulate the first line
+                if char == '.' and isLaserAbove(row, col, lines): # Move laser down
                     lines[row] = insertCinStr(lines[row], col, '|')
-                elif char == '^' and isLaserAbove(row, col, lines):
+                    
+                elif char == '^' and isLaserAbove(row, col, lines): # Perform split
                     splits += 1
                     if col - 1 >= 0 and lines[row][col - 1] == '.':
-                        print("Case 2")
                         lines[row] = insertCinStr(lines[row], col - 1, '|')
+                        
                     if col + 1 < len(line) and lines[row][col + 1] == '.':
-                        print("Case 3")
                         lines[row] = insertCinStr(lines[row], col + 1, '|')
     
     return lines, splits
-                        
+
+memo = {}
+
+def simulateQStep(lines, row, col, splitsAcc):
+    if (row, col) in memo:
+        return memo[(row, col)]
+    
+    lines = lines
+    
+    splits = splitsAcc
+        
+    line = lines[row]
+    
+    char = line[col]
+    
+    #print(f'''Current line: {line}, row: {row}, col: {col}''')
+    
+    if row != len(lines) - 1: # Don't simulate the first line
+        if char == '.': # Move laser down
+            result = simulateQStep(lines, row + 1, col, splits)
+            
+        elif char == '^': # Perform split
+            
+            leftPath = 0
+            rightPath = 0
+
+            if col - 1 >= 0:
+                leftPath = simulateQStep(lines, row, col - 1, splits)
+                
+            if col + 1 < len(line):
+                rightPath = simulateQStep(lines, row, col + 1, splits)
+                
+            result = leftPath + rightPath
+        else:
+            result = 0
+    else: 
+        result =  1 # Base case, one full path found
+        
+    memo[(row, col)] = result # Store pos
+ 
+    return result          
+    
+def findS(line):
+    
+    for c in range(len(line)):
+        if line[c] == 'S':
+            return c
+        
+    print("Error, no start found!!")
+    return -1
         
 def printGraphic(lines):
     for l in lines:
@@ -46,36 +94,18 @@ def printGraphic(lines):
 with open("day_7/input.txt") as f:
     lines = f.read().splitlines()
         
-    printGraphic(lines)
+    #printGraphic(lines)
     
-    print("")
+    #print("")
 
     
-    newLines, splits = simulateStep(lines)
+    #newLines, splits = simulateStep(lines)
     
-    printGraphic(newLines)
-    print(f"Total amount of splits: {splits}")
+    #printGraphic(newLines)
+    #print(f"Part 1: Total amount of splits: {splits}")
     
     
     
-
-
-
-'''
-.......S.......
-...............
-.......^.......
-...............
-......^.^......
-...............
-.....^.^.^.....
-...............
-....^.^...^....
-...............
-...^.^...^.^...
-...............
-..^...^.....^..
-...............
-.^.^.^.^.^...^.
-...............
-'''
+    totalPaths = simulateQStep(lines, 1, findS(lines[0]), 0)
+    
+    print(f"Part 2: Total amount of paths: {totalPaths}")
