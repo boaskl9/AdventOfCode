d8a4485b7253196b8f3ff2f7ee4cc699521c991a
unknown <boaskl9@gmail.com>
2025-12-17 11:37:34 +0100

    inital part of day 10

--- day_10/day_10.py
+++ day_10/day_10.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import time
 from tracemalloc import start
 
+from numpy import character
+
 # Add parent directory to path
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
@@ -14,12 +16,74 @@ doPrint = True
 def Print(str):
     if doPrint: print(str)
     
+idCounter = 0
+
+class Button:
+    def __init__(self, input):
+        self.values = self.buildButton(input)
+        
+    def buildButton(self, input) -> list[str]:
+        return input.replace('(', '').replace(')', '').split(',')
+    
+    def __str__(self):
+        return str(self.values)
+    
+    def getStr(self):
+        return str(self.values)
+        
+
 class Machine:
     def __init__(self, line):
+        global idCounter
+        self.id = idCounter
+        idCounter += 1
+        
+        # Split input line
         elements = line.split(' ')
-        self.indicatorLights = elements[0]
-        self.joltage = elements[len(elements) - 1]
-        self.buttons = elements[1:len(elements) - 2]    
+        
+        self.indicatorLights = self.buildLights(elements[0])
+        
+        self.joltage = self.buildJoltage(elements[len(elements) - 1])
+        
+        self.buttons = self.buildButtons(elements[1:len(elements) - 2])
+        
+    def buildLights(self, lights) -> list[bool]:
+        returnLights = []
+        
+        for c in lights:
+            if c == '[' or c == ']':
+                continue
+            elif c == '.':
+                returnLights.append(False)
+            elif c == '#':
+                returnLights.append(True)
+            else:
+                print(f"ERROR: Invalid char in lights: {c}")
+                
+        return returnLights
+    
+    def buildJoltage(self, jolts) -> list[str]:
+        return jolts.replace('{', '').replace('}', '').split(',')
+    
+    def buildButtons(self, buttons) -> list[Button]:    
+        returnButtons = []
+        
+        for b in buttons:
+            returnButtons.append(Button(b))
+        
+        return returnButtons
+    
+    def printButtons(self):
+        rStr = ""
+        for b in self.buttons:
+            rStr += b.getStr()
+            
+        return rStr
+        
+    def __str__(self) -> str:
+        Print(f"Machine id: {self.id}\nlights: {self.indicatorLights}\nbuttons: {self.printButtons()}\njoltage: {self.joltage}\n")
+        return ""
+        
     
 def solve_part1(lines):
     machines = []
@@ -28,7 +92,8 @@ def solve_part1(lines):
         machines.append(Machine(l))
         
     for m in machines:
-        Print(m.buttons)
+        #Print(m.buttons)
+        Print(m)
     return
 
 
